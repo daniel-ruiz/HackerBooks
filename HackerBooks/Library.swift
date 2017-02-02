66806e19e647953d853b6c9a71146a36f1fc6edb
Daniel Ruiz <danruicar@gmail.com>
2017-02-02 01:12:17 +0100

    Added data retrieval methods to Library class

--- HackerBooks/Library.swift
+++ HackerBooks/Library.swift
@@ -11,9 +11,16 @@ import Foundation
 class Library {
     
     //MARK: - Properties
+    
     var books = MultiDictionary<Tag, Book>()
+    var booksCount: Int {
+        get {
+            return books.countUnique
+        }
+    }
     
     //MARK: - Initiaization
+    
     init(books: [Book]) {
         for book in books {
             for tag in book.tags {
@@ -22,4 +29,25 @@ class Library {
         }
     }
     
+    //MARK: - Data Retrieval
+    
+    func books(forTag tag: Tag) -> [Book]? {
+        if let bookCollection = books[tag] {
+            return Array(bookCollection)
+        } else {
+            return nil
+        }
+    }
+    
+    func bookCount(forTag tag: Tag) -> Int {
+        return books(forTag: tag)?.count ?? 0
+    }
+    
+    func book(forTag tag: Tag, at: Int) -> Book? {
+        if let bookCollection = books(forTag: tag) {
+            return bookCollection[at]
+        } else {
+            return nil
+        }
+    }
 }
